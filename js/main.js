d24fefc3b88ed7b1aec9424e7626355454772f4f
mr-moto <khash615@gmail.com>
2017-01-21 21:32:53 -0800

    added full prod list, implemented slider plugin

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+		$('.prod-list').flickity({
+			cellAlign: 'left',
+			contain: true,
+			percentPosition: false,
+			imagesLoaded: true,
+			autoPlay: true,
+			prevNextButtons: false,
+            wrapAround: true,
+            resize: true,
+		});
+	})  
