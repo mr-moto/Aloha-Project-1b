508bf2766fbaf75ced39ec3e3796c3f42c024168
mr-moto <khash615@gmail.com>
2017-01-22 00:55:45 -0800

    added email validation to form

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
-$(function() {
-		$('.prod-list').flickity({
-			cellAlign: 'left',
-			contain: true,
-			percentPosition: false,
-			imagesLoaded: true,
-			autoPlay: true,
-			prevNextButtons: false,
-            wrapAround: true,
-            resize: true,
-		});
-	})  
+$(function() { 
+
+
+    // flickity slider plugin
+    $('.prod-list').flickity({
+		cellAlign: 'left',
+		contain: true,
+		percentPosition: false,
+		imagesLoaded: true,
+		autoPlay: true,
+		prevNextButtons: false,
+        wrapAround: true,
+        resize: true,
+	});
+
+
+    // validate email function
+    $('form').on('click','button', function(event){
+        event.preventDefault();
+        var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        var val = $('.email-input').val();
+            if(email.test(val)){
+            alert('coolcats)');
+            }
+            else{
+            alert('YOU DUN GOOFED');
+            }
+    });
+
+
+
+})  
